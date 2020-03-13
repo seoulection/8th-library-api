246e1f364a97e5e10f3d0297bbc598597c0aec31
junehoe <br1ansun9@gmail.com>
2020-03-13 01:20:18 -0700

    Implements basic user login
    Authentication WIP

--- lib/eighth_library_api_web/router.ex
+++ lib/eighth_library_api_web/router.ex
@@ -2,14 +2,38 @@ defmodule EighthLibraryApiWeb.Router do
   use EighthLibraryApiWeb, :router
 
   pipeline :api do
-    plug CORSPlug, origin: "https://eighth-library-web.herokuapp.com"
     plug CORSPlug, origin: "http://localhost:3000"
     plug :accepts, ["json"]
+    plug :fetch_session
+  end
+
+  pipeline :api_auth do
+    plug :ensure_authenticated
   end
 
   scope "/api", EighthLibraryApiWeb do
     pipe_through :api
 
-    get "/hello", HelloController, :index
+    post "/users/login", UserController, :login
+  end
+
+  # Protected Routes
+  scope "/api", EighthLibraryApiWeb do
+    pipe_through [:api, :api_auth]
+  end
+
+  # Plug function
+  defp ensure_authenticated(conn, _opts) do
+    current_user_email = get_session(conn, :current_user_email)
+
+    if current_user_email do
+      conn
+    else
+      conn
+      |> put_status(:unauthorized)
+      |> put_view(EighthLibraryApiWeb.ErrorView)
+      |> render("401.json", message: "Unauthenticated user")
+      |> halt()
+    end
   end
 end
